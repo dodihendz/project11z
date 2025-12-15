a7ebacdc9a270e5d12bae4346d6364a914533d8a
duyduyomm <dodiforgame@gmail.com>
2025-12-15 22:28:31 +0700

    feat post

---
+++ app/Models/Post.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Post
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel',
+                'title' => 'Judul Artikel',
+                'author' => 'Jip jipp',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Sed do eiusmod tempor incididunt ut labore et dolore
+                            magna aliqua.',
+                'date' => '12 December 2025'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'kisah-pilu',
+                'title' => 'Kisah pilu',
+                'author' => 'yahh ini dia',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Sed do eiusmod tempor incididunt ut labore et dolore
+                            magna aliqua.',
+                'date' => '11 December 2025'
+            ]
+        ];
+    }
+
+    public static function find($slug): array
+    {
+        $posts = static::all();
+        // return Arr::first($posts, function ($post) use ($slug) {
+        //     return $post['slug'] == $slug;
+        // });
+        $checked =  Arr::first($posts, fn($post) => $post['slug'] == $slug);
+        if (!$checked) {
+            return abort(404);
+        }
+        return $checked;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
@@ -14,28 +15,7 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', [
         "title" => "Blog",
-        'posts' => [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel',
-                'title' => 'Judul Artikel',
-                'author' => 'Jip jipp',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.',
-                'date' => '12 December 2025'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'kisah-pilu',
-                'title' => 'Kisah pilu',
-                'author' => 'yahh ini dia',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.',
-                'date' => '11 December 2025'
-            ]
-        ]
+        'posts' => Post::all()
     ]);
 });
 
@@ -45,34 +25,15 @@ Route::get('/contact', function () {
 
 Route::get('/posts/{slug}', function ($slug) {
     // dd($id); //debugging to see if the id is captured correctly
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel',
-            'title' => 'Judul Artikel',
-            'author' => 'Jip jipp',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.',
-            'date' => '12 December 2025'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'kisah-pilu',
-            'title' => 'Kisah pilu',
-            'author' => 'yahh ini dia',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.',
-            'date' => '11 December 2025'
-        ]
-    ];
+    // $posts = Post::all();
 
     // $post = collect($posts)->firstWhere('id', $id);
     // $post = Arr::first($posts, fn ($post) => $post['id'] == $id); //arrow function
-    $post = Arr::first($posts, function ($post) use ($slug) {          //anonymous function and must using 'use' to access $id variable
-        return $post['slug'] == $slug;
-    });
+    // $post = Arr::first($posts, function ($post) use ($slug) {          //anonymous function and must using 'use' to access $id variable
+    //     return $post['slug'] == $slug;
+    // });
+
+    $post = Post::find($slug);
 
     // dd($post); //debugging data already ok or not
     return view('post', [
