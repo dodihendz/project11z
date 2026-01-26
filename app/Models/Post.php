cd956b475e440bbf79c0a91d8974dc36ed2f3c88
duyduyomm <dodiforgame@gmail.com>
2026-01-26 21:44:01 +0700

    searching

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -18,5 +19,38 @@ class Post extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function scopeSearch(Builder $query, array $filters): void
+    {
+        // if ($filters['search'] ?? false) {
+        //     $query->where('title', 'like', '%' . request('search') . '%');
+        // }
+        // $query->when($filters['search'] ?? false, function ( $query, $search) {
+        //     $query->where('title', 'like', '%' . request('search') . '%');
+        // });
+        $query->when(
+            $filters['search'] ?? false,
+            fn($query, $search) =>
+            $query->where('title', 'like', '%' . $search . '%')
+        );
+        $query->when(
+            $filters['category'] ?? false,
+            fn($query, $category) =>
+            $query->whereHas(
+                'category',
+                fn($query) =>
+                $query->where('slug', $category)
+            )
+        );
+        $query->when(
+            $filters['author'] ?? false,
+            fn($query, $author) =>
+            $query->whereHas(
+                'author',
+                fn($query) =>
+                $query->where('username', $author)
+            )
+        );
     }
 }
